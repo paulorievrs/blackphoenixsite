a373c1d0a4e84eac0555a1a5d154f8435a79430e
Paulo Rievrs <paulorievrsoliveira@gmail.com>
2021-01-06 17:17:12 -0300

    removendo fotter

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,10 @@
 
     </body>
-<footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-        <span class="text-muted">Entre em contato pelo e-mail: <a target="_blank" href="mailto:contato@blackphoenix.com.br">contato@blackphoenix.com.br</a>.</span>
-    </div>
-</footer>
+{{--<footer class="footer mt-auto py-3 bg-light">--}}
+{{--    <div class="container">--}}
+{{--        <span class="text-muted">Entre em contato pelo e-mail: <a target="_blank" href="mailto:contato@blackphoenix.com.br">contato@blackphoenix.com.br</a>.</span>--}}
+{{--    </div>--}}
+{{--</footer>--}}
 </html>
 
 @yield('footer')
